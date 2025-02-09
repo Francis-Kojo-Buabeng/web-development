44ec9b1593518605245f54254e181b1b680b89e3
Francis Kojo Buabeng <myallcodes1@gmail.com>
2025-02-09 02:07:43 +0000

    fetch method learning

--- JavaScript/Fetch Method/currency.js
+++ JavaScript/Fetch Method/currency.js
@@ -1,19 +1,45 @@
 document.addEventListener('DOMContentLoaded',function(){
+          //Synchronous way
+     // fetch("https://api.exchangeratesapi.io/latest?base=USD")
+     // .then(response => {
+     //      if (!response.ok){
+     //           console.log(`There is a problem during fetch ${response.status}`);
+     //      }else{
+     //           return response.json();
+     //      }
+     // })
+     // .then(data =>{
+     //      console.log(data);
+     // })
 
-     fetch("https://api.exchangeratesapi.io/latest?base=USD")
-     .then(response => {
-          if (!response.ok){
-               console.log(`There is a problem during fetch ${response.status}`);
-          }else{
-               return response.json();
-          }
-     })
-     .then(data =>{
-          console.log(data);
-     })
+     // .catch(error => {
+     //      console.error("There has been error in fetch operation: ", error)
+     // });
 
-     .catch(error => {
-          console.error("There has been error in fetch operation: ", error)
-     });
+     // fetch("https://jsonplaceholder.typicode.com/posts")
+     // .then(response => response.json())
+     // .then(data=> {
+     //      const apiDataBody = data[0].body;
+     //      document.querySelector('body').innerHTML = apiDataBody;
+     // })
+     // .catch(err=> console.log("Error: Something bad happened"))
+
+
+          //Asynchronous way
+   async function getApi() {
+     try{
+          const response = await fetch('https://randomuser.me/api/');
+          const data = await response.json();
+          console.log(data)
+     }
+     catch(error){
+          console.error(`Error: ${error.status}`);
+     }
      
-});
+
+   }
+
+   getApi();
+ });
+
+
